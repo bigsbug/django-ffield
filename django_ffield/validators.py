7ef23145c4555d82f7edbb95d9e164799657788e
bigsbug <booxgitboox@gmail.com>
2023-09-17 13:27:38 +0330

    improve validator by addiing new feature and improve code

--- django_ffield/validators.py
+++ django_ffield/validators.py
@@ -10,16 +10,23 @@ class FileTypeValidator:
     """A file type validator
     """
 
-    def __init__(self, types: list[str]) -> None:
+    def __init__(self, allowd_types: list[str],disallowed_types=[]) -> None:
         """
         Parameters
         ----------
-        types : list[str]
+        allowed_types : list[str]
             types and sub-types that allowd file be
             example :
                 `["image","pdf","mp4",...]`
+        
+        disallowed_types : list[str]
+            types and sub-types that disallowed file be
+            example :
+                `["py","exe","bat",...]`
+        
         """
-        self.types = list(map(str.lower, set(types)))
+        self.allowd_types = list(map(str.lower, set(allowd_types)))
+        self.disallowed_types = list(map(str.lower, set(disallowed_types)))
 
     def __call__(self, file:FileIO) -> None:
         """called by django validator
@@ -35,7 +42,13 @@ class FileTypeValidator:
             when the file don't match by selected types this error raises to show to user
         """
         typ, formt = meme_type(file)
-        if typ not in self.types and formt not in self.types:
+        
+        # * to prevent forbidden upload files by user
+        if typ not in self.allowd_types and formt not in self.allowd_types:
             raise ValidationError(
-                f"Unsupported Format: The {formt} type is not supported. Supported formats: {self.types}."
+                f"Unsupported Format: The {formt} type is not in allowd supported formats. Supported formats: {self.allowd_types}. Unsupported formats: {self.disallowed_types}. "
+            )
+        elif typ in self.disallowed_types or formt in self.disallowed_types :
+            raise ValidationError(
+                f"Unsupported Format: The {formt} type is not supported formats. Unsupported formats: {self.disallowed_types}."
             )
